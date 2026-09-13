80557d6ebb56d479b0af7b83682e7d32fb8d333a
AdaVercatti <mjr83098@gmail.com>
2026-09-14 00:32:53 +0800

    add Link instead of a Element

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
 import './App.css'
 import {Routes,Route} from 'react-router'
+import OrdersPage from './pages/OrdersPage'
 function App() {
   return (
     <>
     <Routes>
       <Route path='/' element={<HomePage></HomePage>}></Route>
       <Route path='/checkout' element={<CheckoutPage></CheckoutPage>}></Route>
+      <Route path='/orders' element={<OrdersPage></OrdersPage>}></Route>
     </Routes>
     </>
   )
