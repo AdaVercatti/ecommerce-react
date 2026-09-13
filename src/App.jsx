5bbcb24457d1b1197a6eabc3d203b4f13a23704a
AdaVercatti <mjr83098@gmail.com>
2026-09-13 22:13:15 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import HomePage from './pages/HomePage'
+import CheckoutPage from './pages/CheckoutPage'
 import './App.css'
-
+import {Routes,Route} from 'react-router'
 function App() {
   return (
     <>
-     <HomePage></HomePage>
+    <Routes>
+      <Route path='/' element={<HomePage></HomePage>}></Route>
+      <Route path='/checkout.html' element={<CheckoutPage></CheckoutPage>}></Route>
+    </Routes>
     </>
   )
 }
